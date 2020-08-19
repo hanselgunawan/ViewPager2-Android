0859b87b073727d46d835c026375e88161950822
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-08-18 17:46:27 -0700

    added viewpager2

--- app/src/main/java/com/hanseltritama/viewpager2demo/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/viewpager2demo/MainActivity.kt
@@ -2,11 +2,38 @@ package com.hanseltritama.viewpager2demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.activity_main.*
+import me.relex.circleindicator.CircleIndicator3
 
 class MainActivity : AppCompatActivity() {
+
+    private val titleList = mutableListOf<String>()
+    private val descriptionList = mutableListOf<String>()
+    private val imagesList = mutableListOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        postToList()
+
+        my_view_pager.adapter = ViewPagerAdapter(titleList, descriptionList, imagesList)
+        my_view_pager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+
+        val indicator = findViewById<CircleIndicator3>(R.id.circle_indicator)
+        indicator.setViewPager(my_view_pager)
+    }
+
+    private fun addToList(title: String, description: String, image: Int) {
+        titleList.add(title)
+        descriptionList.add(description)
+        imagesList.add(image)
+    }
+
+    private fun postToList() {
+        for (i in 1..5) {
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher_round)
+        }
     }
 }
